2c76a714ce05fab0e261d7cfaac200a5d1f9c266
alasdair.mccall@gmail.com <alasdair.mccall@gmail.com>
2012-12-09 21:44:07 +0000

    First commit

---
+++ auth.rb
@@ -0,0 +1,27 @@
+require 'rubygems'
+require 'sinatra'
+
+helpers do
+
+  def protected!
+    unless authorized?
+      response['WWW-Authenticate'] = %(Basic realm="Restricted Area")
+      throw(:halt, [401, "Not authorized\n"])
+    end
+  end
+
+  def authorized?
+    @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+    @auth.provided? && @auth.basic? && @auth.credentials && @auth.credentials == ['admin', 'admin']
+  end
+
+end
+
+get '/' do
+  "Everybody can see this page"
+end
+
+get '/protected' do
+  protected!
+  "Welcome, authenticated client"
+end
---
+++ datamappering.rb
@@ -0,0 +1,96 @@
+require 'rubygems'
+require 'sinatra'
+require 'data_mapper' # metagem, requires common plugins too.
+require 'json'
+require 'fileutils'
+require 'securerandom'
+
+# need install dm-sqlite-adapter
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/blog.db")
+
+class Post
+    include DataMapper::Resource
+    property :id, Serial
+    property :title, String
+    property :body, Text
+    property :created_at, DateTime
+end
+
+class User
+	include DataMapper::Resource
+	property :id, Serial
+	property :deviceToken, String
+	property :userToken, String
+	property :email, String
+	property :created_at, DateTime
+	property :last_modified, DateTime
+end
+
+# Perform basic sanity checks and initialize all relationships
+# Call this when you've defined all your models
+DataMapper.finalize
+
+# automatically create the post table
+Post.auto_upgrade!
+User.auto_upgrade!
+
+get '/user/:userId' do
+
+	@user = User.get([:userId])
+
+	erb :user
+end
+
+post '/user', :provides => :json do
+
+	request.body.rewind
+	data = JSON.parse request.body.read
+
+	#would be nice to do some validatio here
+
+	newUserToken = SecureRandom.hex(16)
+	
+	@user = User.create(
+		:deviceToken => "#{data['deviceToken']}",
+		:userToken => "#{newUserToken}",
+		:email => "#{data['email']}", #email isn't needed
+		:created_at => Time.now,
+		:last_modified => Time.now
+	)
+
+	"#{newUserToken}"
+
+	@user.save
+end
+
+
+post '/createPost' do
+
+	request.body.rewind  # in case someone already read it
+  	data = JSON.parse request.body.read
+
+	# create makes the resource immediately
+	@post = Post.create(
+	  :title      => "#{data['title']}",
+	  :body       => "#{data['body']}",
+	  :created_at => Time.now
+	)
+
+	@post.save                           # persist the resource
+end
+
+
+# application
+
+put '/upload/:id' do
+  File.open(params[:id], 'w+') do |file|
+    file.write(request.body.read)
+  end
+end
+
+
+get '/' do
+    # get the latest 20 posts
+    @posts = Post.all(:order => [ :id.desc ], :limit => 20)
+    erb :index
+end
