5f2cad06f13c3c5f0ff0c94609483ce099cd2e91
alasdair.mccall@gmail.com <alasdair.mccall@gmail.com>
2012-12-09 23:21:38 +0000

    CRUD working for device/user levels

--- datamappering.rb
+++ datamappering.rb
@@ -11,11 +11,12 @@ DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/yt.db")
 class User
 	include DataMapper::Resource
 	property :id, Serial
+	property :appId, String
 	property :deviceToken, String
 	property :userToken, String
 	property :email, String
-	property :created_at, DateTime
-	property :last_modified, DateTime
+	property :created, DateTime
+	property :lastModified, DateTime
 end
 
 # Perform basic sanity checks and initialize all relationships
@@ -25,53 +26,108 @@ DataMapper.finalize
 # automatically create the post table
 User.auto_upgrade!
 
+#APPLICATION
+
+#-------------------
+# Device
+
+get '/device/:deviceToken' do
+
+	user = User.first(:deviceToken => params[:deviceToken])
+
+	if user == nil
+		status 404
+		body "User record with deviceToken[#{params[:deviceToken]}] not found. Try registering a device first."
+	else 
+		status 200
+		content_type :json
+		{:lastModified => "#{user.lastModified}"}.to_json
+	end
+
+end
+
+put '/device', :provides => :json do
+
+	request.body.rewind
+	data = JSON.parse request.body.read
+
+	#would be nice to do some validatio here
+	newToken = SecureRandom.hex(16)
+
+	user = User.create(
+		:deviceToken => "#{newToken}",
+		:appId => "#{data['appId']}",
+		:created => Time.now,
+		:lastModified => Time.now
+	)
+
+	user.save
+
+	status 201
+	content_type :json
+	{:deviceToken => "#{newToken}", :lastModified => "#{user.lastModified}"}.to_json
+end
+
+#-------------------
+# User
+
 get '/user/:userId' do
+
+	@user = User.get(params[:userId])
+
+	if @user == nil
+		status 404
+		body "User with id[#{params[:userId]}] not found. Try registering a user first."
+	else 
+		status 200
+		content_type :json
+		{:userToken => "#{@user.userToken}", :email => "#{@user.email}", :lastModified => "#{@user.lastModified}"}.to_json
+	end
+
+end
+
+get '/admin/user/:userId' do
 
 	@user = User.get(params[:userId])
 
 	erb :user
 end
 
-post '/user', :provides => :json do
+get '/admin/users' do
+	@users = User.all
+
+	erb :users
+end
+	
+
+put '/user', :provides => :json do
 
 	request.body.rewind
 	data = JSON.parse request.body.read
 
-	#would be nice to do some validatio here
+	#would be nice to do some validation here
 
-	newUserToken = SecureRandom.hex(16)
-	
-	@user = User.create(
-		:deviceToken => "#{data['deviceToken']}",
-		:userToken => "#{newUserToken}",
-		:email => "#{data['email']}", #email isn't needed
-		:created_at => Time.now,
-		:last_modified => Time.now
-	)
+	user = User.first(:deviceToken => "#{data['deviceToken']}")
 
-	"#{newUserToken}"
+	if user == nil
+		status 400
+		body "Attempting to register a user without first registering a device for deviceToken[#{data['deviceToken']}]. Try registering a device first."
 
-	@user.save
+	else
+		newToken = SecureRandom.hex(16)
+
+		user.update(:userToken => "#{newToken}", :email => "#{data['email']}" , :lastModified => Time.now)	
+
+		user.save
+
+		#return the JSON response
+		status 201
+		content_type :json
+		{:userToken => "#{newToken}", :lastModified => "#{user.lastModified}"}.to_json
+	end
 end
 
-
-post '/createPost' do
-
-	request.body.rewind  # in case someone already read it
-  	data = JSON.parse request.body.read
-
-	# create makes the resource immediately
-	@post = Post.create(
-	  :title      => "#{data['title']}",
-	  :body       => "#{data['body']}",
-	  :created_at => Time.now
-	)
-
-	@post.save                           # persist the resource
-end
-
-
-# application
+#upload PMS
 
 put '/upload/:id' do
   File.open(params[:id], 'w+') do |file|
